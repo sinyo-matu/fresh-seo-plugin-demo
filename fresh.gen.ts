2df3313e2aeeba26eeb15aeeff952ba973829810
sinyo-matu <sinyo_matu@icloud.com>
2022-10-15 23:25:52 +0900

    up fresh-seo meta feature

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,6 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/about.tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/sitemap.xml.ts";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -14,7 +13,6 @@ const manifest = {
     "./routes/about.tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
-    "./routes/sitemap.xml.ts": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
