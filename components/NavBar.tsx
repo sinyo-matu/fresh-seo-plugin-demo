62600a9d3979478bcf0b2d6e0c029701f2dca371
sinyo-matu <sinyo_matu@icloud.com>
2022-10-15 23:41:54 +0900

    up fresh-seo meta feature

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,8 @@
-
 export default function NavBar() {
   return (
     <nav class="flex gap-3">
-        <a href="/">index/</a>
-        <a href="/about">about/</a>
+      <a href="/">index/</a>
+      <a href="/about">about/</a>
     </nav>
-  )
+  );
 }
